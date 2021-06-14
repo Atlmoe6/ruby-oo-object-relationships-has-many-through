691473a1e5b663bcf383ea8dbe53ca9365947289
Doe Mohamud <doe.mahamud1@gmail.com>
2021-06-13 21:22:15 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
+# ./lib/waiter.rb
 class Waiter
 
-end
+    attr_accessor :name, :yrs_experience
+  
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+      end 
+      def meals
+        Meal.all.select do |meal|
+          meal.customer == self
+        end
+    def waiters
+         meals.map do |meal|
+          meal.waiter
+     end
+        
+  end 
